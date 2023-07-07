68c8b0e063d424c712702434bd421aa901be463e
Nick Evans <nick.evans@northwestern.edu>
2023-07-06 20:33:13 -0500

    Command to do the bottery

--- app/Console/Commands/MakeFediBotPosts.php
+++ app/Console/Commands/MakeFediBotPosts.php
@@ -2,29 +2,44 @@
 
 namespace App\Console\Commands;
 
+use App\Domains\FediBot\Models\Bot;
+use App\Domains\FediBot\PostingService;
 use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
 
 class MakeFediBotPosts extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:make-fedi-bot-posts';
+    protected $signature = 'fedi-bot:post {--force}';
+    protected $description = 'Checks bot backends for new content & posts it.';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
-    /**
-     * Execute the console command.
-     */
-    public function handle()
+    public function handle(PostingService $postingService)
     {
-        //
+        $force = (bool) $this->option('force');
+
+        $bots = ! $force
+            ? $postingService->botsToCheck()
+            : Bot::all();
+
+        $results = [];
+        foreach ($bots as $bot) {
+            $activity = $postingService->post($bot);
+
+            $results[] = [
+                $bot->username,
+                Arr::get($activity->toArray(), 'newPosts.count'),
+                $bot->next_check_at
+            ];
+        }
+
+        if ($bots->count() > 0) {
+            $this->table(['Bot', 'Posts Made', 'Next Check'], $results);
+        } else {
+            $this->info('No bots to check!');
+            $this->newLine();
+
+            $this->table(['Bot', 'Next Check'], Bot::all()->map(fn (Bot $bot) => [$bot->username, $bot->next_check_at]));
+        }
+
+        return self::SUCCESS;
     }
 }
