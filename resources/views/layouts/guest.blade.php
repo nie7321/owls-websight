091e117e6aa78942ccf1e5956a0e4f8f295b334f
Nick Evans <nick.evans@northwestern.edu>
2024-02-11 13:53:50 -0600

    the balance of light & darkness

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="scroll-smooth">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ isset($title) ? "{$title} | owls" : "owls" }}</title>
+    <script type="text/javascript">
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    </script>
     @vite('resources/css/app.css')
+    @livewireStyles
 </head>
 <body class="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
 <section class="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
@@ -36,12 +44,53 @@
                               d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
                     </svg>
                 </button>
-                <button aria-label="Toggle Dark Mode">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                         class="h-6 w-6 text-gray-900 dark:text-gray-100">
+                <button :aria-label="`toggle ${currentMode} mode`"
+                        x-data="{
+                            currentMode: null,
+                            mode() {
+                                if (document.documentElement.classList.contains('dark')) {
+                                    return 'dark';
+                                }
+
+                                if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                                    return 'dark';
+                                }
+
+                                return 'light';
+                            },
+                            toggleMode() {
+                                newMode = this.mode() === 'light' ? 'dark' : 'light';
+                                localStorage.theme = newMode;
+
+                                document.documentElement.classList.remove('dark', 'light');
+                                document.documentElement.classList.add(newMode);
+                                this.currentMode = newMode;
+                            },
+                            init() {
+                                this.currentMode = this.mode();
+                            }
+                        }"
+                        @click="toggleMode"
+                        x-cloak
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 20 20"
+                         fill="currentColor"
+                         class="h-6 w-6 text-gray-900 dark:text-gray-100"
+                         x-show="currentMode == 'light'"
+                    >
                         <path fill-rule="evenodd"
                               d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
                               clip-rule="evenodd"></path>
+                    </svg>
+                        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 20 20" fill="currentColor"
+                         class="h-6 w-6 text-gray-900 dark:text-gray-100"
+                         x-show="currentMode == 'dark'"
+                    >
+                        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
                     </svg>
                 </button>
                 <button aria-label="Toggle Menu" class="sm:hidden">
@@ -127,5 +176,6 @@
 
 @stack('modals')
 @vite('resources/js/app.js')
+@livewireScripts
 </body>
 </html>
