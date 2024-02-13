0ae10bd4762a524620a2da4480437c3aba67368b
Nick Evans <nick.evans@northwestern.edu>
2024-02-13 14:40:07 -0600

    Trigger initialization w/out spamming fedi

--- app/Filament/Resources/BotResource/Pages/CreateBot.php
+++ app/Filament/Resources/BotResource/Pages/CreateBot.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\BotResource\Pages;
 
+use App\Domains\FediBot\Models\Bot;
+use App\Domains\FediBot\PostingService;
 use App\Filament\Resources\BotResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
@@ -9,4 +11,15 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateBot extends CreateRecord
 {
     protected static string $resource = BotResource::class;
+
+    protected function afterCreate(): void
+    {
+        /** @var Bot $bot */
+        $bot = $this->record;
+
+        /** @var PostingService $postingService */
+        $postingService = resolve(PostingService::class);
+
+        $postingService->initializeWithoutPosting($bot);
+    }
 }
