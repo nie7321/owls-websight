6f62ea73b73eb796e7ecb78b04256234c6e0397c
Nick Evans <nick.evans@northwestern.edu>
2024-02-18 12:58:52 -0600

    Revamp contacts

---
+++ resources/views/components/contact-entry.blade.php
@@ -0,0 +1,10 @@
+@props(['label', 'icon'])
+<div class="w-[45%] mt-2 flex align-text-bottom">
+    <div class="basis-6 flex align-text-bottom">
+        <x-dynamic-component :component="$icon" class="fill-current text-gray-700 dark:text-gray-200 w-4"/>
+        <span class="sr-only">{{ $label }}</span>
+    </div>
+    <div>
+        {{ $slot }}
+    </div>
+</div>
