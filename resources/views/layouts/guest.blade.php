6b482f02fe6f4c4a5750168d49f161aa02a6b3a9
Nick Evans <nick.evans@northwestern.edu>
2024-02-18 07:38:20 -0600

    Fix RSS icon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -171,7 +171,7 @@
                     </a>
                     <a class="text-sm text-gray-500 transition hover:text-gray-600" target="_blank" href="{{ route('feed.atom') }}">
                         <span class="sr-only">RSS feed</span>
-                        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
+                        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
                              class="fill-current text-gray-700 hover:text-primary-500 dark:text-gray-200 dark:hover:text-primary-400 h-6 w-6">
                         >
                             <path d="M19.199 24C19.199 13.467 10.533 4.8 0 4.8V0c13.165 0 24 10.835 24 24h-4.801zM3.291 17.415c1.814 0 3.293 1.479 3.293 3.295 0 1.813-1.485 3.29-3.301 3.29C1.47 24 0 22.526 0 20.71s1.475-3.294 3.291-3.295zM15.909 24h-4.665c0-6.169-5.075-11.245-11.244-11.245V8.09c8.727 0 15.909 7.184 15.909 15.91z"/>
