df8cb20dac280446f63f085c5acb8d002d3f6b3c
Nick Evans <nick.evans@northwestern.edu>
2024-11-30 11:46:37 -0600

    Self-hosted DID

--- config/social.php
+++ config/social.php
@@ -4,6 +4,6 @@ return [
     'subdomain' => env('APP_SOCIAL_SUBDOMAIN', 'owls.godless-internets.org'),
     'redirect' => env('APP_SOCIAL_REDIRECT_URL', env('APP_URL')),
     'atproto' => [
-        'did' => env('ATPROTO_DID_PLC', 'did:plc:7cnk4air2fjpbrlmti4ogqs4'),
+        'did' => env('ATPROTO_DID_PLC', 'did:plc:3cs2crtlwbbu5ui7ybqv2zlx'),
     ]
 ];
