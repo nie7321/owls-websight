e6243157d7b86d8e29d8794376c84ab74ccb90f1
Nick Evans <nick.evans@northwestern.edu>
2026-08-07 06:42:32 -0500

    OPML updated @ 8/6/2026 17:22:51

--- app/Domains/RssDiscovery/Entities/FeedDiscoveryResult.php
+++ app/Domains/RssDiscovery/Entities/FeedDiscoveryResult.php
@@ -67,6 +67,7 @@ readonly class FeedDiscoveryResult
             'https://devz.cl' => 'https://devz.cl/index.xml',
             'https://substack.com/@thethinkingboard' => 'https://1beautifulproblem.substack.com/feed',
             'https://aeish-world.neocities.org/blog_front' => 'https://aeish-world.neocities.org/feed.txt',
+            'https://heartsoulmachine.com/' => 'https://heartsoulmachine.com/feed.xml',
             /*
             'https://mattbee.zone' => 'https://mattbee.zone/rss.xml',
             'https://www.achilletoupin.com' => 'https://www.achilletoupin.com/feed.xml',
