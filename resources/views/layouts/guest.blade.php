f39da007200b7d67584cdb083da665b7f9d99f63
Nick Evans <nick.evans@northwestern.edu>
2026-08-24 16:28:00 -0500

    Include a rel=me link for indieauth

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,9 @@
     <meta property="og:url" content="{{ route('blog-post.index') }}" />
     <meta property="og:site_name" content="{{ config('app.name') }}" />
     <meta property="og:locale" content="en_US" />
+
     <meta name="fediverse:creator" content="@owls@yshi.org" />
+    <link rel="me" href="mailto:nick@godless-internets.org">
 
     @isset($title)
         <meta property="og:title" content="{{ $title }}" />
